38746a66a944fa711dd24fd9a61052d6b186e4e9
20161104580 <a20161104580@Mac15.local>
2018-09-12 11:18:11 +0800

    01
    01

---
+++ 001/001/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  001
+//
+//  Created by 20161104580 on 18/9/12.
+//  Copyright © 2018年 20161104580. All rights reserved.
+//
+
+import Foundation
+
+var sum=0
+for index in 0...100{
+    sum+=index
+    print("sum=\(sum)")
+}
+print("最终结果： \(sum)")
+
