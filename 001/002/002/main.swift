55e2e55e67967a03c82178f46284f698590f2aff
20161104580 <a20161104580@Mac15.local>
2018-09-19 10:39:02 +0800

    03
    排序

--- 001/002/002/main.swift
+++ 001/002/002/main.swift
@@ -10,9 +10,9 @@ import Foundation
 
 var numbers = [5,4,6,3,7,8,1,2,9]
 var x = 0
-for i in 0..<10{
-    for j in 0..<(10-i-1){
-        if numbers[j] < numbers[j+1]{
+for i in 0..<numbers.count{
+    for j in 0..<(numbers.count-i-1){
+        if numbers[j] > numbers[j+1] {
             x = numbers[j]
             numbers[j] = numbers[j+1]
             numbers[j+1] = x
