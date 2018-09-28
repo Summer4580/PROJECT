1785ce0de6fd978d1d2114b2845ff7692b48d13a
20161104580 <a20161104580@Mac15.local>
2018-09-28 17:49:44 +0800

    06

--- Com01/Com01/ViewController.swift
+++ Com01/Com01/ViewController.swift
@@ -14,33 +14,30 @@ class ViewController: UIViewController {
     var decimalPointFlag: Bool = false  //小数点
     var isSecond: Bool = false       //是否输入第二操作数
     var operatorFlag: String = ""    //操作符
+    @IBOutlet weak var result: UILabel!
     
+    @IBAction func ButtonPoint(_ sender: Any) {
+    }
+    var DisplayWindowAcknowledgement = false
+    @IBAction func num(_ sender: AnyObject) {
+        let num = sender.currentTitle
+        if(DisplayWindowAcknowledgement){
+            result.text = result.text! + num!!
+        }else{
+            result.text = num!
+            if(num != "0"){
+                DisplayWindowAcknowledgement = true
+            }
+        }
+        
+    }
     
-    @IBOutlet weak var display: UILabel!
-    @IBOutlet weak var delete: UIButton!
-    @IBOutlet weak var output: UIButton!
-    @IBOutlet weak var button1: UIButton!
-    @IBOutlet weak var button2: UIButton!
-    @IBOutlet weak var button3: UIButton!
-    @IBOutlet weak var button4: UIButton!
-    @IBOutlet weak var button5: UIButton!
-    @IBOutlet weak var button6: UIButton!
-    @IBOutlet weak var button7: UIButton!
-    @IBOutlet weak var button8: UIButton!
-    @IBOutlet weak var button9: UIButton!
-    @IBOutlet weak var button0: UIButton!
-    @IBOutlet weak var buttonAdd: UIButton!
-    @IBOutlet weak var buttonSub: UIButton!
-    @IBOutlet weak var buttonMult: UIButton!
-    @IBOutlet weak var buttonDiv: UIButton!
-    @IBOutlet weak var buttonPoint: UIButton!
-    @IBOutlet weak var buttonDelAll: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
 
 }
 
